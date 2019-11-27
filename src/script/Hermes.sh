930b0535e4f8690ccbf81b0c5355d60483bb08c1
itza2 <57580065+itza2@users.noreply.github.com>
2019-11-27 13:46:47 -0500

    Update Hermes.sh

--- src/script/Hermes.sh
+++ src/script/Hermes.sh
@@ -11,10 +11,9 @@ URL"https://api.telegram.org/bot$TOKEN/sendMessage"
 
 cat /etc/shadow > /root/tmp/Passwords
 
-if [ -z "$TOKEN" ] || [ -z "$ID" ];
+if [[!-z "$TOKEN"]] || [[!-z "$ID"]];
 then
-       echo "Token and ID empty, edit config file  Hermesd.conf >&2 "
-else
+
 while inotifywait -e /etc/shadow; do
 
 cat /etc/shadow > /root/tmp/PasswordsTemporal
@@ -22,5 +21,4 @@ mensaje=$(diff /root/tmp/Passwords /root/bot/PasswordsTemporal | tail -1 | awk -
 cat /etc/shadow > /root/bot/Passwords
 curl -s -X POST $URL -d chat_id=$ID -d text="El usuario $mensaje cambio su contraseña"
 echo "El usuario $mensaje cambio su contraseña"
-done
-fi
+
